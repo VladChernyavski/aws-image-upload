1fee78f70da0728223b4d618b5333ec3054a1029
VladChernyavski <chernyavskivl@gmail.com>
2021-10-10 10:34:21 +0300

    init project

---
+++ src/test/java/by/chernyavski/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package by.chernyavski.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
