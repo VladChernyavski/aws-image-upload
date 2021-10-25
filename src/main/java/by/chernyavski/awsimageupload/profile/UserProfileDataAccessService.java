8e202fba3d38034ad46632e0fb03d3feafab34a0
VladChernyavski <chernyavskivl@gmail.com>
2021-10-25 20:38:59 +0300

    API & service layers

---
+++ src/main/java/by/chernyavski/awsimageupload/profile/UserProfileDataAccessService.java
@@ -0,0 +1,21 @@
+package by.chernyavski.awsimageupload.profile;
+
+import by.chernyavski.awsimageupload.datastore.FakeUserProfileDataStore;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserProfileDataAccessService {
+
+    private final FakeUserProfileDataStore fakeUserProfileDataStore;
+
+    public UserProfileDataAccessService(FakeUserProfileDataStore fakeUserProfileDataStore) {
+        this.fakeUserProfileDataStore = fakeUserProfileDataStore;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return fakeUserProfileDataStore.getUserProfiles();
+    }
+
+}
---
+++ src/main/java/by/chernyavski/awsimageupload/profile/UserProfileService.java
@@ -0,0 +1,20 @@
+package by.chernyavski.awsimageupload.profile;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfiles() {
+        return userProfileDataAccessService.getUserProfiles();
+    }
+
+}
