f100fdd50ff40006e8a9f98134fa5e3f882a0aab
VladChernyavski <chernyavskivl@gmail.com>
2021-11-04 19:56:20 +0300

    rendering user profile

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import axios from 'axios';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const UserProfiles = () => {
+    const [userProfiles, setUserProfiles] = useState([]);
+
     const fetchUserProfiles = () => {
         axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
             console.log(res);
+            setUserProfiles(res.data);
         });
     };
 
     useEffect(() => {
         fetchUserProfiles();
     }, []);
-    return <h1>Hello</h1>
+
+    return userProfiles.map((userProfile, index) => {
+        return (
+            <div key={index}>
+                <h1>{userProfile.username}</h1>
+                <p>{userProfile.userProfileId}</p>
+            </div>
+        );
+    });
 };
 
 function App() {
